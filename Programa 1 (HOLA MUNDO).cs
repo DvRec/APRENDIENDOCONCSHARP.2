c1f4d7a2e124df25742a830fc5e3abae7f2e887a
DvRec <56002741+DvRec@users.noreply.github.com>
2019-10-01 19:36:26 -0500

    DERECHO A EXAMEN
    10 PROGRAMAS DERECHO A EXAMEN

---
+++ Programa 1 (HOLA MUNDO).cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROGRAMA_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Para agregar comentarios utilizamos el doble slash "//"
+
+            Console.WriteLine("Hola mundo, ahora en C#");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Programa 4 (ENTRADA).cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Entrada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Primero declaramos la variable que vamos a usar
+            string valor;
+            //Ahora nos piden la variable
+            Console.Write("\n\n\tEscribe algo: ");
+            //Luego se lee y se uarda la variable
+            valor = Console.ReadLine();
+            //Se hace un try catch que es mas facil de usar y con menos problemas que con el if else
+            try
+            {
+                int x = int.Parse(valor);
+                Console.WriteLine("\n\tDato entero {0} es correcto.", valor);
+            }
+            catch
+            {
+                Console.WriteLine("\n\tDato no es entero. Intentar de nuevo.");
+            }
+            //Y finalmente se acaba el programa
+            Console.WriteLine("\n\tPresiona enter para continuar....");
+            //Se hace un ReadKey ya que sin el no se veria el programa solo correria sin pausarse
+            Console.ReadKey();
+        }
+    }
+}
